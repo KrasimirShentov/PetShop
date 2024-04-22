6b730da932bc52b5e019b019262febf08dc2d8db
KrasimirSh <krasimirshentov@gmail.com>
2024-04-22 15:58:48 +0300

    Polishing Employee controller

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PetShop.Petshop.Models;
-using PetShop.Petshop.Repositories.Repositories;
+using PetShop.Petshop.services.Interfaces;
 
 namespace PetShop.Controllers
 {
@@ -10,19 +10,19 @@ namespace PetShop.Controllers
     public class EmployeeController : ControllerBase
     {
         private readonly ILogger<EmployeeController> _logger;
-        private readonly EmployeeRepository _employeeRepository;
+        private readonly IEmployeeService _employeeService;
 
-        public EmployeeController(ILogger<EmployeeController> logger, EmployeeRepository employeeRepository)
+        public EmployeeController(ILogger<EmployeeController> logger, IEmployeeService employeeRepository)
         {
             _logger = logger;
-            _employeeRepository = employeeRepository;
+            _employeeService = employeeRepository;
         }
 
         [HttpGet("{employeeID}")]
         public async Task<IActionResult> GetEmployeeByIDAsync(int employeeID)
         {
-            var employee = await _employeeRepository.GetEmployeeByIDAsync(employeeID);
-            
+            var employee = await _employeeService.GetEmployeeByIdAsync(employeeID);
+
             if (employee == null)
             {
                 return NotFound();
@@ -33,28 +33,31 @@ namespace PetShop.Controllers
             }
         }
 
-        [HttpGet]
+        [HttpGet("Get all")]
         public async Task<IActionResult> GetAllEmployees()
         {
-            var employees = await _employeeRepository.GetAllEmployeesAsync();
+            var employees = await _employeeService.GetAllEmployeesAsync();
             return Ok();
         }
 
+        [HttpPost("Add employee")]
         public async Task<IActionResult> AddEmployee(Employee employee)
         {
-            await _employeeRepository.AddEmployeeAsync(employee);
+            await _employeeService.AddEmployeeAsync(employee);
             return Ok();
         }
 
+        [HttpDelete("Delete employee")]
         public async Task<IActionResult> DeleteEmployee(int employeeID)
         {
-            await _employeeRepository.DeleteEmployeeAsync(employeeID);
+            await _employeeService.DeleteEmployeeAsync(employeeID);
             return Ok();
         }
 
+        [HttpPut("Update employee")]
         public async Task<IActionResult> UpdateEmployee(Employee employee)
         {
-            await _employeeRepository.UpdateEmployeeAsync(employee);
+            await _employeeService.UpdateEmployeeAsync(employee);
             return Ok();
         }
     }
