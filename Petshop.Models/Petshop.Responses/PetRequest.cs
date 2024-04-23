5d2fb52a835fa0df98f3be8584f7769bcede09de
KrasimirSh <krasimirshentov@gmail.com>
2024-04-23 20:10:46 +0300

    Creating PetController with CRUD operations

--- Petshop.Models/Petshop.Responses/PetRequest.cs
+++ Petshop.Models/Petshop.Responses/PetRequest.cs
@@ -8,7 +8,7 @@ namespace PetShop.Petshop.Models.Petshop.Responses
         public int PetID { get; set; }
 
         public string Breed { get; set; }
-
+        public string Name { get; set; }
         public int Age { get; set; }
 
         public PetType PetType { get; set; }
